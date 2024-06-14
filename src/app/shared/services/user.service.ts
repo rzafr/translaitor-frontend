5b52685b3c31f3db539538b51d221a4cf9b369d4
rzafr <rubenzafraramirez@gmail.com>
2024-06-14 16:46:45 +0200

    environment.prod

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../models/user.model';
 
 @Injectable({
